fcfbaca0eea821343f0cd31f21cb6e826b1d2b13
nabeelarain713 <nabeelarain713@gmail.com>
2023-07-31 03:05:43 +0500

    first commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "7fa447ad49f58cce91689ee853016b17",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
